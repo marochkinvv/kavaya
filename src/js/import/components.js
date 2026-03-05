218dc53ce49167c90b263963d822ebad8be4990f
marochkinvv <marochkinvv@gmail.com>
2026-03-06 00:41:23 +0300

    ready contavts page, fixed button fize, add favicon, delete vendors.js

--- src/js/import/components.js
+++ src/js/import/components.js
@@ -1,1 +1,4 @@
 import '%components%/header-footer-wrapper/header-footer-wrapper';
+import '%components%/button/button';
+import '%components%/input/input';
+import '%components%/news-card/news-card';
--- src/js/import/pages.js
+++ src/js/import/pages.js
@@ -1,2 +1,5 @@
-import "%pages%/main/main";
-import "%pages%/business/business";
+import '%pages%/main/main';
+import '%pages%/business/business';
+import '%pages%/article/article';
+import '%pages%/news/news';
+import '%pages%/contacts/contacts';
